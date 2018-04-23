ed2891dd48f57051cc55246c04eb3c9151db1e16
Maame Yaa Osei <maameyaa.osei@ashesi.edu.gh>
2018-04-23 18:37:42 +0000

    Add files via upload

---
+++ Database.sql
@@ -0,0 +1,7 @@
+Create Database Project2;
+Use Project2;
+Create Table TShirts(name varchar(25), price int, availability bit);
+Create Table Trousers(name varchar(25), price int, availability bit);
+Create Table Shorts(name varchar(25), price int, availability bit);
+Create Table Skirts(name varchar(25), price int, availability bit);
+Drop Table TShirts;
